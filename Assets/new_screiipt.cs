8fece98534b5ff23184ceba8e7bb6df6995055ce
skyflyer55 <jlecoq@eatel.net>
2014-10-29 13:02:36 -0500

    BOOM

---
+++ Assets/new_screiipt.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class new_screiipt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Debug.Log("ASAAWQWQWQWQWQ");
+        //dsasadsadsa
+        //asdasd
+
+	}
+}
